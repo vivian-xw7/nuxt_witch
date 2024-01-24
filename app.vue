648394facdeb26bcfb2d8fcc049cd6e91eabcd6e
Vivian Hayes <vivian.xw7@gmail.com>
2024-01-23 22:05:47 -0600

    add views and components

--- app.vue
+++ app.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- <AppHeader /> -->
+
+  <!-- <router-view /> -->
 </template>
+
+<script>
+// import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    // AppHeader,
+  },
+  mounted() {
+    
+    // ASCII art
+    const owl = `
+      /\\︵-︵/\\
+      |(◉)(◉)|
+      \\ ︶V︶ /
+      /↺↺↺↺\\
+      ↺↺↺↺↺|
+      \\↺↺↺↺/
+      ¯¯/\\¯¯/\\¯¯`;
+
+    const iSeeYouText = `Thanks for visiting my site! You can find more projects in my GitHub: https://github.com/vivian-xw7`;
+
+    console.log(owl);
+    console.log(iSeeYouText);
+  },
+};
+</script>
+
+<style scoped>
+/* @import "./styles/typography.css";
+@import "./styles/btn.css";
+@import "./styles/grid.css";
+@import "./styles/animations.css";
+@import "./assets/main.css"; */
+</style>
